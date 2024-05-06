27f032f0da67bf72fff8eeeb5611e698b842f857
Angel Veliz <99308699+angelveliz1837@users.noreply.github.com>
2024-05-06 14:51:10 -0500

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,18 @@ import { EditTipoVehiculoComponent } from './componentes/tipoVehiculo/edit-tipo-
 import { EditMarcaComponent } from './componentes/marca/edit-marca/edit-marca.component';
 import { ListarMarcaComponent } from './componentes/marca/listar-marca/listar-marca.component';
 import { AddMarcaComponent } from './componentes/marca/add-marca/add-marca.component';
+import { ListarTipoDocumentoComponent } from './componentes/tipoDocumento/listar-tipo-documento/listar-tipo-documento.component';
+import { AddTipoDocumentoComponent } from './componentes/tipoDocumento/add-tipo-documento/add-tipo-documento.component';
+import { EditTipoDocumentoComponent } from './componentes/tipoDocumento/edit-tipo-documento/edit-tipo-documento.component';
+import { ListarEstacionamientoComponent } from './componentes/estacionamiento/listar-estacionamiento/listar-estacionamiento.component';
+import { AddEstacionamientoComponent } from './componentes/estacionamiento/add-estacionamiento/add-estacionamiento.component';
+import { EditEstacionamientoComponent } from './componentes/estacionamiento/edit-estacionamiento/edit-estacionamiento.component';
+import { ListarRolComponent } from './componentes/rol/listar-rol/listar-rol.component';
+import { AddRolComponent } from './componentes/rol/add-rol/add-rol.component';
+import { EditRolComponent } from './componentes/rol/edit-rol/edit-rol.component';
+import { ListarEmpleadoComponent } from './componentes/empleado/listar-empleado/listar-empleado.component';
+import { AddEmpleadoComponent } from './componentes/empleado/add-empleado/add-empleado.component';
+import { EditEmpleadoComponent } from './componentes/empleado/edit-empleado/edit-empleado.component';
 
 export const routes: Routes = [
     {path:'vehiculos', component:ListarVehiculoComponent},
@@ -24,5 +36,18 @@ export const routes: Routes = [
     {path:'editarTipoVehiculo',component:EditTipoVehiculoComponent},
     {path:'marcas', component:ListarMarcaComponent},
     {path:'nuevoMarca', component:AddMarcaComponent},
-    {path:'editarMarca',component:EditMarcaComponent}
+    {path:'editarMarca',component:EditMarcaComponent},
+    {path:'tipoDocumentos', component:ListarTipoDocumentoComponent},
+    {path:'nuevoTipoDocumento', component:AddTipoDocumentoComponent},
+    {path:'editarTipoDocumento',component:EditTipoDocumentoComponent},
+    {path:'estacionamientos', component:ListarEstacionamientoComponent},
+    {path:'nuevoEstacionamiento', component:AddEstacionamientoComponent},
+    {path:'editarEstacionamiento',component:EditEstacionamientoComponent},
+    {path:'roles', component:ListarRolComponent},
+    {path:'nuevoRol', component:AddRolComponent},
+    {path:'editarRol',component:EditRolComponent},
+    {path:'empleados', component:ListarEmpleadoComponent},
+    {path:'nuevoEmpleado', component:AddEmpleadoComponent},
+    {path:'editarEmpleado',component:EditEmpleadoComponent}
+
 ];
